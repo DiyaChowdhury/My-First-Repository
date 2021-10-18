016423051868832d135a41795066aad2c4e82420
DiyaChowdhury <73558242+DiyaChowdhury@users.noreply.github.com>
2021-10-18 17:23:54 -0400

    TYWLS -2023

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hola Chicas</h1>
-      <h1></h1>
+      <h2>Young Women's Leadership School - 2023</h2>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
